5a55d48ab4b26d212e92abf08ca1c772784a4a3f
Paulo <paulohenrique1019@gmail.com>
2023-10-04 19:32:22 -0300

    aplicação de senha segura em java

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Password password = new Password("Paulo1019%");
-        System.out.println("A senha é: " + password.getPassword());
+        Password password1 = new Password("Paulo1019%");
+        Password password2 = new Password("Saulo0193#");
+        System.out.println("A senha é: " + password1.getPassword());
+        System.out.println("A senha é: " + password2.getPassword());
 
     }
 }
